930c5f5b84c2963fa1bb18e40a2a3b3d563c2d7c
Gabriel Evangelista <gabriel.evan.queiroz@gmail.com>
2018-01-07 02:07:08 -0200

    JWT token generate, token access

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ import {Express} from  'express';
 
 import * as fs from 'fs';
 import * as https from 'https';
+import {handleAuthentication} from './auth';
+import {handleAutorization} from './authz'
 
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -14,6 +16,10 @@ server.use(middlewares)
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
+
+//middleware para login
+server.post('/login', handleAuthentication)
+server.use('/orders', handleAutorization)
 
 
 // Use default router
